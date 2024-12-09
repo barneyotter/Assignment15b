c2de5d37b272bf20872a025115faec7affecb9ba
barneyotter <128318050+barneyotter@users.noreply.github.com>
2024-12-08 21:11:10 -0500

    Blank main template with header

--- Assignment15b/main.cpp
+++ Assignment15b/main.cpp
@@ -1,14 +1,2 @@
-//
-//  main.cpp
-//  Assignment15b
-//
-//  Created by Maia B on 12/8/24.
-//
-
-#include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
-}
+// Maia Barnhart
+// CIS 1202 501
